7c948dd9e6759bc6e0ad2c4d183bd5c3538f3154
MA <93675964+mrcamelia@users.noreply.github.com>
2021-11-23 12:39:00 +0700

    Update javascrpt.js

--- javascrpt.js
+++ javascrpt.js
@@ -1,13 +1,8 @@
 var i;
-var acrdon = document.getElementsByClassName("accordion").length;
-var ans = document.getElementsByClassName("answer");
+var right = document.getElementsByClassName("qna");
 
-for (i=0;i<acrdon;i++){
-    acrdon[i].addEventListener("click", function(){
-    if (ans[i].style.display =="none"){
-        document.querySelector.style.display =="block";
-    } else {
-        document.querySelector.style.display =="block";
-    }   
+for (i=0;i<right.length;i++){
+right[i].addEventListener('click', function(){
+    this.classList.toggle('active');
     });
 }
